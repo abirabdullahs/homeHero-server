7bf41fbcc180e51ac5254fc3356639780ed38486
Abir Abdullah <abirabdullah3491@gmail.com>
2025-11-13 08:53:34 +0600

    index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+       // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
 
@@ -82,7 +82,7 @@ async function run() {
 
 
 
-        
+
 
         app.get('/services/:email', async (req, res) => {
             const email = req.params.email;
