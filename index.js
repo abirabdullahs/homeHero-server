c8d6d2f2d68e609fdf5d7abda3809b41b5e07047
Abir Abdullah <abirabdullah3491@gmail.com>
2025-11-13 09:01:12 +0600

    create route

--- index.js
+++ index.js
@@ -20,12 +20,19 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
-       // await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
 
